058724d81bda59bc478ce89d20fdcb1d02685d44
Karrie Garzelloni <kgarzelloni@yahoo.com>
2022-01-23 16:06:59 -0700

    add functions updated

--- db/seed.sql
+++ db/seed.sql
@@ -12,7 +12,7 @@ VALUES  ("Sales Lead", 100000,1),
         ("Account Manager", 100000,3),
         ("Accountant", 100000,3),
         ("Legal Team Lead",100000,4),
-        ("Lawer",100000,4);
+        ("Lawyer",100000,4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ("John", "Doe",1, null),
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const db = require("./db/sqlconnection");
 const logo = require("asciiart-logo");
 const config = require("./package.json");
 
-
 // initial function that runs everything to start the app
 
 function init() {
@@ -24,145 +23,233 @@ function initialPrompts() {
         name: "options",
         message: "What would you like to do?",
         choices: [
-          "View all employees",
-          "Add an employee",
-          "Update an Employee Role",
-          "View all roles",
-          "Add a role",
-          "View all departments",
-          "Add a department",
-          "I'm finished managing this database",
+          { name: "View all employees", value: "view_employees" },
+          { name: "Add an employee", value: "add_employee" },
+          { name: "Update an Employee Role", value: "update_employee" },
+          { name: "View all roles", value: "view_roles" },
+          { name: "Add a role", value: "add_role" },
+          { name: "View all departments", value: "view_departments" },
+          { name: "Add a department", value: "add_department" },
+          { name: "I'm finished managing this database", value: "quit" },
         ],
       },
     ])
     .then((res) => {
-      console.log(res);
+      console.log(res.options);
+      switch (res.options) {
+        case "view_employees":
+          viewEmployees();
+          break;
+        case "view_departments":
+          viewDepartments();
+          break;
+        case "view_roles":
+          viewRoles();
+          break;
+        case "add_employee":
+          addEmployee();
+          break;
+        case "update_employee":
+          updateEmployee();
+          break;
+        case "add_role":
+          addRole();
+          break;
+        case "add_department":
+          addDepartment();
+          break;
+        case "quit":
+          quit();
+          break;
+      }
     });
 }
 
 // function for view employees
-  
-db.query('SELECT * FROM employee_tracker_db.employee', function (err, results) {
-  if(err)console.log(err)
-  console.table(results);
-});
+function viewEmployees() {
+  db.query(
+    "SELECT * FROM employee_tracker_db.employee",
+    function (err, results) {
+      if (err) console.log(err);
+      console.table(results);
+    }
+  );
+  initialPrompts();
+}
 
 // function for add employees
-
-addEmployeePrompt = inquirer.prompt([
-{
-  type: "input",
-  name: "first_name",
-  message: "What is the employee's first name?",
-  
-},
-{
-  type: "input",
-  name: "last_name",
-  message: "What is the employee's last name?",
-  
-},
-{
-  type: "input",
-  name: "role_id",
-  message: "What is the employee's role ID?",
-  
-},
-{
-  type: "input",
-  name: "manager_id",
-  message: "What is the employee's manager ID?",
-  
-},
-
-])
-.then((answers) => {
-  // Use answers for updating employee table, possible solution- switch/case statments to sort through functions depending on user choice
-})
-.catch((error) => {
-  if (error) {
-    // Prompt couldn't be rendered in the current environment
-  } else {
-    // Something else went wrong
-  }
-});
-initialPrompts();
-
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the employee's first name?",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is the employee's last name?",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "What is the employee's role ID?",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "What is the employee's manager ID?",
+      },
+    ])
+    .then((answers) => {
+      db.query(
+        `INSERT INTO employee_tracker_db.employee (first_name, last_name, role_id, manager_id)
+      VALUES (?,?,?,?)`,
+        [
+          answers.first_name,
+          answers.last_name,
+          answers.role_id,
+          answers.manager_id,
+        ]
+      );
+    })
+    .then(() => initialPrompts())
+    .catch((error) => {
+      if (error) {
+      } else {
+      }
+    });
+}
 
 // function for view roles
-
-db.query('SELECT * FROM employee_tracker_db.role', function (err, results) {
-  if(err)console.log(err)
-  console.table(results);
-});
-initialPrompts();
-
-
-// function for add roles
-
-addRolePrompt = inquirer.prompt([
-  {
-    type: "input",
-    name: "new role",
-    message: "What is the name of the new role?",
-    
-  },
-  {
-    type: "input",
-    name: "salary",
-    message: "What is the salary of the new role?",
-    
-  },
-  {
-    type: "input",
-    name: "department_id",
-    message: "What is the department ID for the new role?",
-    
-  },
-  
-  ])
-  .then((answers) => {
-    // Use answers for updating role table
-  })
-  .catch((error) => {
-    if (error) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+function viewRoles() {
+  db.query("SELECT * FROM employee_tracker_db.role", function (err, results) {
+    if (err) console.log(err);
+    console.table(results);
   });
   initialPrompts();
+}
+
+// function for add roles
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the name of the new role?",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the salary of the new role?",
+      },
+      {
+        type: "input",
+        name: "department_id",
+        message: "What is the department ID for the new role?",
+      },
+    ])
+    .then((answers) => {
+      db.query(
+        `INSERT INTO employee_tracker_db.role (title, salary, department_id)
+      VALUES (?,?,?)`,
+        [answers.title, answers.salary, answers.department_id]
+      );
+    })
+    .then(() => initialPrompts())
+    .catch((error) => {
+      if (error) {
+      } else {
+      }
+    });
+}
 
 // function for updating employee role
+function updateEmployee() {
+  
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "select employee",
+        message: "Which employee do you want to update?",
+        choices: [
+           {name: "John Doe"},
+           {name: "Ashely Rodriquez"},
+           {name: "Kevin Tupik"},
+           {name: "Kunal Singh"},
+           {name: "Malia Brown"},
+           
+        ],
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "What is the employee's new role id?",
+      },
+    ])
+    .then((answers) => {
+      db.query(
+        `UPDATE employee_tracker_db.employee (role_id)
+    VALUES (?)`,
+        [answers.role_id]
+      );
+    })
+    .then(() => initialPrompts())
+    .catch((error) => {
+      if (error) {
+      } else {
+      }
+    });
+}
 
 // function for view departments
 
-db.query('SELECT * FROM employee_tracker_db.department', function (err, results) {
-  if(err)console.log(err)
-  console.table(results);
-});
+function viewDepartments() {
+  db.query(
+    "SELECT * FROM employee_tracker_db.department",
+    function (err, results) {
+      if (err) console.log(err);
+      console.table(results);
+    }
+  );
+  initialPrompts();
+}
 
 // function for add department
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "department_name",
+        message: "What is the name of the new department?",
+      },
+      {
+        type: "input",
+        name: "department_id",
+        message: "What is the id number of the new department?",
+      },
+    ])
+    .then((answers) => {
+      db.query(
+        `INSERT INTO employee_tracker_db.department (department_name)
+      VALUES (?, ?)`,
+        [answers.department_name, answers.department_id]
+      );
+    })
+    .then(() => initialPrompts())
+    .catch((error) => {
+      if (error) {
+        console.log("an error occured");
+      } else {
+        console.log("that was successful, feels good doesn't it?");
+      }
+    });
+}
 
-addDepartmentPrompt = inquirer.prompt([
-  {
-    type: "input",
-    name: "new department",
-    message: "What is the name of the new department?",
-    
-  },
-  ])
-  .then((answers) => {
-    // Use answers for updating role table
-  })
-  .catch((error) => {
-    if (error) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
-  initialPrompts();
-
-
-
+function quit() {
+  process.exit();
+}
